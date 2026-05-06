664d5d5c47c3e04e74a879f80e20774915e10071
Alexei <alexei-2006@bk.ru>
2026-05-06 20:39:34 +0600

    moved tests into separate functions

--- stack_test.c
+++ stack_test.c
@@ -4,8 +4,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int main(void) {
-  {
+void test_new_stack_is_empty_and_peek_pop_return_null(void) {
     Stack s = st_new();
 
     assert(st_is_empty(&s));
@@ -13,9 +12,9 @@ int main(void) {
     assert(!st_pop(&s));
 
     st_free(&s);
-  }
+}
 
-  {
+void test_push_one_element_then_pop(void) {
     Stack s = st_new();
 
     assert(st_push(&s, 777));
@@ -26,47 +25,47 @@ int main(void) {
     assert(st_is_empty(&s));
 
     st_free(&s);
-  }
+}
 
-  {
+void test_push_multiple_elements_check_lifo_order(void) {
     Stack s = st_new();
     const int N = 20;
 
     for (int i = 0; i < N; i++)
-      assert(st_push(&s, i));
+        assert(st_push(&s, i));
 
     for (int i = N - 1; i >= 0; i--) {
-      assert(*st_peek(&s) == i);
-      assert(st_pop(&s));
+        assert(*st_peek(&s) == i);
+        assert(st_pop(&s));
     }
 
     assert(st_is_empty(&s));
     st_free(&s);
-  }
+}
 
-  {
+void test_push_many_elements_check_size_and_peek(void) {
     Stack s = st_new();
     const int N = 10000;
 
     for (int i = 0; i < N; i++)
-      assert(st_push(&s, i * 10));
+        assert(st_push(&s, i * 10));
 
     assert(s.size == (size_t)N);
     assert(*st_peek(&s) == (N - 1) * 10);
 
     st_free(&s);
-  }
+}
 
-  {
+void test_empty_after_complete_pop_resets_capacity_and_content(void) {
     Stack s = st_new();
     const int N = 64;
 
     for (int i = 0; i < N; i++)
-      assert(st_push(&s, i));
+        assert(st_push(&s, i));
 
     for (int i = N - 1; i >= 0; i--) {
-      assert(*st_peek(&s) == i);
-      assert(st_pop(&s));
+        assert(*st_peek(&s) == i);
+        assert(st_pop(&s));
     }
 
     assert(st_is_empty(&s));
@@ -74,9 +73,9 @@ int main(void) {
     assert(s.content == NULL);
 
     st_free(&s);
-  }
+}
 
-  {
+void test_push_on_full_stack_fails(void) {
     Stack s = st_new();
 
     size_t max_elements = SIZE_MAX / sizeof(int);
@@ -88,7 +87,14 @@ int main(void) {
 
     s.size = 0;
     st_free(&s);
-  }
+}
 
-  return 0;
+int main(void) {
+    test_new_stack_is_empty_and_peek_pop_return_null();
+    test_push_one_element_then_pop();
+    test_push_multiple_elements_check_lifo_order();
+    test_push_many_elements_check_size_and_peek();
+    test_empty_after_complete_pop_resets_capacity_and_content();
+    test_push_on_full_stack_fails();
+    return 0;
 }
